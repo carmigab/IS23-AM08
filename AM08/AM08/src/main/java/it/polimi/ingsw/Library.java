5995e8a8895ae9b9ab6905400ef50b9edda92590
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-12 17:07:59 +0100

    Library attributes & methods

--- AM08/AM08/src/main/java/it/polimi/ingsw/Library.java
+++ AM08/AM08/src/main/java/it/polimi/ingsw/Library.java
@@ -2,7 +2,15 @@
 
 
 
+public class Library{
+    private Card myLibrary[6][5];
 
+
+    public void add(Card c, int col){};
+    public boolean isFull(){};
+    public int evaluateGroupPoints(){};
+
+}
 
 
 

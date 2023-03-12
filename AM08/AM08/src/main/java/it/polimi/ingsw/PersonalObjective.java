6073282b163895e8cd3da5134db25f31b9afb185
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-12 17:25:33 +0100

    PersonalObjective attributes & methods

---
+++ AM08/AM08/src/main/java/it/polimi/ingsw/PersonalObjective.java
@@ -0,0 +1,40 @@
+
+
+
+
+public class PersonalObjective {
+
+    private SingleObjective personalObj[];
+
+
+    public SingleObjective[] getObjective(){};
+    public int evaluate(){};
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

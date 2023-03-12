b36c7102f27fc6f707f1e81892357d02a6b3d12f
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-12 16:46:37 +0100

    Game model methods

--- AM08/AM08/src/main/java/it/polimi/ingsw/GameModel.java
+++ AM08/AM08/src/main/java/it/polimi/ingsw/GameModel.java
@@ -12,6 +12,30 @@ public class GameModel {
 
 
 
+    private void makeMove(int x, int y, int column){};
+    public boolean checkValidMove(int pos[][], int num){};
+
+
+    public void evaluatePoints(){};
+    private void evaluatePOPoints(){};
+    private void evaluateGroupPoints(){};
+    private void evaluateCOPoints(){};
+    private void evaluateFirstPoints(){};
+
+
+    private boolean boardToBeFilled(){};
+    public boolean nextTurn(){};
+
+
+    public String getWinner(){};
+
+
+
+
+
+
+
+
 
 
 

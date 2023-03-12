95e39da63c03e70433316b9fab607fba37f68408
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-12 16:22:59 +0100

    Game Model file + first attributes

---
+++ AM08/AM08/src/main/java/it/polimi/ingsw/GameModel.java
@@ -0,0 +1,68 @@
+
+
+public class GameModel {
+    private int numPlayers;
+    private PlayerState playersList;
+    private GameBoard gameBoard;
+    private CommonObjectives commonObjs[];
+    private PersonalObjectives personalObjs[];
+    private int currentPlayer;
+    private boolean isLastTurn;
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

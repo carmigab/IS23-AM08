37dc849dc2a5e6ceef26bfd186c5b62a2fc29972
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-12 17:28:11 +0100

    SingleObjective attributes & methods

--- AM08/AM08/src/main/java/it/polimi/ingsw/SingleObjective.java
+++ AM08/AM08/src/main/java/it/polimi/ingsw/SingleObjective.java
@@ -5,7 +5,22 @@
 
 
 
+public class SingleObjective {
+    private int position[];
+    private CardColor color;
 
+
+    public int[] getPosition(){};
+    public CardColor getColor(){};
+
+
+
+
+
+
+
+
+}
 
 
 

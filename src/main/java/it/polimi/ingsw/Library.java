88dbc76ee0fcf96777584631e4b12f92a7357f13
gabricarm <gabriele.carminati@mail.polimi.it>
2023-03-14 13:53:20 +0100

    readded classes scheletons

---
+++ src/main/java/it/polimi/ingsw/Library.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw;
+import it.polimi.ingsw.AppConstants.*;
+
+public class Library{
+    private Card myLibrary[][];
+
+    public void add(Card c, int col){};
+    public boolean isFull(){};
+    public int evaluateGroupPoints(){};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/it/polimi/ingsw/SingleObjective.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw;
+import java.util.List;
+
+public class SingleObjective {
+    private List<Integer> position;
+    private CardColor color;
+
+
+    public List<Integer> getPosition(){};
+    public CardColor getColor(){};
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

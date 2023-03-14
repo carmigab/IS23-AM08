13593994842fa4bb1f83317264ac4129c5a8af53
gabricarr <gabriele.carrino@mail.polimi.it>
2023-03-14 19:03:36 +0100

    First half of PlayerState methods

--- src/main/java/it/polimi/ingsw/PlayerState.java
+++ src/main/java/it/polimi/ingsw/PlayerState.java
@@ -6,23 +6,96 @@ public class PlayerState {
     private Library myLibrary;
     private int POPoints;
     private int COPoints;
-    private int firstPoint;
     private int groupPoints;
-    private PersonalObjective PersonalObj;
+    private int firstPoint;
+
+    private PersonalObjective personalObj;
     private boolean comObjDone[];
 
 
 
-    public String getNickname(){};
-    public Library getLibrary(){};
-    public int getPoints(){};
-    public boolean[] getCODone(){};
+    /**
+     * This is the constructor
+     * @param name: the nickname of the player
+     * @param Obj: the personal objective of the player
+     */
+    public PlayerState(String name, PersonalObjective Obj){
+        this.nickname = name;
+        this.personalObj = Obj;
+
+        // Initialization
+        this.myLibrary = new Library();
+        this.POPoints = 0;
+        this.COPoints = 0;
+        this.groupPoints = 0;
+        this.firstPoint = 0;
+
+        this.comObjDone = new boolean[2];      // Initialized to false
+    }
 
 
-    public void addCOPoints(int p){};
-    private void setPOPoints(int p){};
-    private void setGroupPoints(int p){};
+    /**
+     * This method returns the nickname
+     * @return nickname
+     */
+    public String getNickname(){
+        return this.nickname;
+    };
+
+
+    /**
+     * This method returns the Library
+     * @return myLibrary
+     */
+    public Library getLibrary(){
+        return this.myLibrary;
+    };
+
+
+    /**
+     * This method returns the total points of the player
+     * @return total points
+     */
+    public int getPoints(){
+        return POPoints + COPoints + groupPoints + firstPoint;
+    };
+
+
+    /**
+     * This method returns the array that tells if a commonObj is done or not
+     * @return array that tells if a commonObj is done or not
+     */
+    public boolean[] getCODone(){
+        return comObjDone;
+    };
+
+
+    /**
+     * This methods adds points to COPoints counter
+     * @param p : the points to add
+     */
+    public void addCOPoints(int p){
+        this.COPoints += p;
+    };
+
+    /**
+     * This method sets the POPoints counter
+     * @param p : the points to set the PoPoints counter to
+     */
+    private void setPOPoints(int p){
+        this.POPoints = p;
+    };
+
+
+    /**
+     * This method sets the groupPoints counter
+     * @param p : the points to set the groupPoints counter to
+     */
+    private void setGroupPoints(int p){
+        this.groupPoints = p;
+    };
     public void setFirstPoints(){};
+
     public void setCODone(int pos){};
 
 

65a8e2b14d87e50d0688d3f288ed14b459b65028
mattecenz <matteo.cenzato@mail.polimi.it>
2023-03-21 15:29:49 +0100

    fixed test of gamemodel

--- src/test/java/it/polimi/ingsw/model/GameModelTest.java
+++ src/test/java/it/polimi/ingsw/model/GameModelTest.java
@@ -13,19 +13,19 @@ import java.util.List;
 
 class GameModelTest {
 
-//    @Test
-//    public void testPersistency() throws IOException {
-//        List<String> players=new ArrayList<>(4);
-//        players.add("MatteCenz"); players.add("GabriCarr"); players.add("GabriCarm"); players.add("AleCappe");
-//        GameModel gm=new GameModel(4, players);
-//        Gson gs=new GsonBuilder().setPrettyPrinting().create();
-//        String fileToSave="src/main/config/model/fileToSave.json";
-//        FileWriter fr=new FileWriter(fileToSave);
-//        String stringToSave=gs.toJson(gm);
-//        System.out.println(stringToSave);
-//        fr.write(stringToSave);
-//        fr.close();
-//        assertTrue(true);
-//    }
+    @Test
+    public void testPersistency() throws IOException {
+        List<String> players=new ArrayList<>(4);
+        players.add("MatteCenz"); players.add("GabriCarr"); players.add("GabriCarm"); players.add("AleCappe");
+        GameModel gm=new GameModel(4, players);
+        Gson gs=new GsonBuilder().setPrettyPrinting().create();
+        String fileToSave="src/main/config/model/fileToSave.json";
+        FileWriter fr=new FileWriter(fileToSave);
+        String stringToSave=gs.toJson(gm);
+        System.out.println(stringToSave);
+        fr.write(stringToSave);
+        fr.close();
+        assertTrue(true);
+    }
 
 }

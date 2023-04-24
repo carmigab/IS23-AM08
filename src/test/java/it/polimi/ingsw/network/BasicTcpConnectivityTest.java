8326ae1ff9e20da5e489394b40b398e7b70ddfda
gabricarr <gabriele.carrino@mail.polimi.it>
2023-04-24 14:53:49 +0200

    added basic test for Tcp connection

--- src/test/java/it/polimi/ingsw/network/BasicTcpConnectivityTest.java
+++ src/test/java/it/polimi/ingsw/network/BasicTcpConnectivityTest.java
@@ -1,35 +1,40 @@
 package it.polimi.ingsw.network;
 
+
 import it.polimi.ingsw.dummies.FakeView;
 import it.polimi.ingsw.network.client.Client;
 import it.polimi.ingsw.network.client.RmiClient;
+import it.polimi.ingsw.network.client.TcpClient;
 import it.polimi.ingsw.network.server.LobbyServer;
 import it.polimi.ingsw.network.server.constants.ServerConstants;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BasicConnectivityTest {
 
+public class BasicTcpConnectivityTest {
+
+    @Disabled
     @Test
-    public void basicRmiClientServerConnectionTest(){
+    public void basicTcpClientServerConnectionTest(){
         boolean testSuccessful = true;
         try {
             LobbyServer lobbyS = new LobbyServer();
             lobbyS.start();
 
-            String nickname = "TheOneWhoTests";
+            String nickname = "TheOneWhoTestsTcp";
             FakeView fakeView = new FakeView();
 
             //System.out.println("Starting client");
-            Client client = new RmiClient(nickname, fakeView, "localhost", ServerConstants.RMI_PORT);
+            Client client = new TcpClient(nickname, fakeView, "localhost", ServerConstants.TCP_PORT);
 
             client.chooseNickname(nickname);
 
             client.createGame(2);
         } catch (Exception e) {
             testSuccessful = false;
-            e.printStackTrace();
+            // e.printStackTrace();
         }
 
         assertTrue(testSuccessful);

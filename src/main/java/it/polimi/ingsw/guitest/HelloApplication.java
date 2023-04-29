dac0a2e954d8745c38569d2c8121c52d42bf1ec3
mattecenz <matteo.cenzato@mail.polimi.it>
2023-04-29 17:20:13 +0200

    added gui scene to insert rmi/tcp , ip and port of server

--- src/main/java/it/polimi/ingsw/guitest/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/guitest/HelloApplication.java
@@ -3,7 +3,11 @@ package it.polimi.ingsw.guitest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
@@ -26,9 +30,13 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader=new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene=new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Scene scene=new Scene(fxmlLoader.load());
+        scene.setOnKeyPressed((key)->{
+            if(key.getCode().equals(KeyCode.ENTER)) ((HelloController)fxmlLoader.getController()).onConnectButtonClick();
+        });
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon_50x50px.png"))));
+        stage.setTitle("MYSHELFIE");
         stage.setScene(scene);
         stage.show();
     }
